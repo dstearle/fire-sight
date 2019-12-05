19818bf82868001c3e51ace60683ec8629f3f7c0
dstearle <dallas.st.earle@gmail.com>
2019-12-04 18:33:03 -0800

    Fixed dashboard not working when logged in

---
+++ database/migrations/2019_12_05_015230_add_user_id_to_posts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserIdToPosts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Adds user id
+            $table->integer('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // Deletes user id
+            $table->dropColumn('user_id');
+        });
+    }
+}
