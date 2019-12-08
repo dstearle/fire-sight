15afa62d6870502ae7dd8d690dd8d271d08a2839
dstearle <dallas.st.earle@gmail.com>
2019-12-07 23:17:52 -0800

    Moved dashboard.blade.php to pages folder and reconfigured the DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -26,6 +26,6 @@ class DashboardController extends Controller
     {
         $user_id = auth()->user()->id;
         $user = User::find($user_id);
-        return view('dashboard')->with('posts', $user->posts);
+        return view('pages.dashboard')->with('posts', $user->posts);
     }
 }
