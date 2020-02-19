a85b2fbd76b0f1bf264df7f6900240ce28f4f36f
dstearle <dallas.st.earle@gmail.com>
2020-02-18 19:33:50 -0800

    Created and executed migration for profile_picture in user table and fixed migration for cover_image

--- database/migrations/2020_02_19_032903_add_profile_picture_to_users.php
+++ database/migrations/2020_02_19_032903_add_profile_picture_to_users.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddCoverImageToPosts extends Migration
+class AddProfilePictureToUsers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class AddCoverImageToPosts extends Migration
      */
     public function up()
     {
-        Schema::table('posts', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             // Adds column for cover images
-            $table->string('cover_image');
+            $table->string('profile_picture');
         });
     }
 
@@ -26,9 +26,9 @@ class AddCoverImageToPosts extends Migration
      */
     public function down()
     {
-        Schema::table('posts', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             // Removes column for cover images
-            $table->dropColumn('cover_image');
+            $table->dropColumn('profile_picture');
         });
     }
 }
