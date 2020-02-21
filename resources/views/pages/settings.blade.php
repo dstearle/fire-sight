d34afe5de8f4f858a44a2ac20193d9ba14acabb6
dstearle <dallas.st.earle@gmail.com>
2020-02-20 16:07:17 -0800

    Began working on the form for editing the user profile under settings

--- resources/views/pages/settings.blade.php
+++ resources/views/pages/settings.blade.php
@@ -27,17 +27,67 @@
 
                     <div class="row">
 
-                        {{-- Table Title --}}
+                        {{-- Form Title --}}
                         <h3 class="col-8">Profile Settings</h3>
 
-                        {{-- Create Posts Link --}}
                         <div class="col-4">
 
                         </div>
 
                     </div>
 
-                    {{--  --}}
+                    {{-- Form --}}
+                    <div class="form">
+
+                        {{-- User Name --}}
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="register_name" type="text" class="form-control @error('register_name') is-invalid @enderror" name="name" value="{{ old('register_name') }}" required autocomplete="name" autofocus>
+
+                                @error('register_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- Email --}}
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="register_email" type="email" class="form-control @error('register_email') is-invalid @enderror" name="email" value="{{ old('register_email') }}" required autocomplete="email">
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        {{-- Profile Picture --}}
+                        <div class="row justify-content-center">
+
+                            <div>
+
+                                <img
+                                    class="row" 
+                                    style="width: 8rem; height: 8rem; border-radius: 50%;" 
+                                    src="/storage/profile_pictures/{{ $user->profile_picture }}" 
+                                    alt=""
+                                >
+
+                                <h6 class="row">Profile Picture: </h6>
+
+                            </div>
+
+                        </div>
+
+                    </div>
 
                 </div>
 
